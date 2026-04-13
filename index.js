66d2cce08028a811961c7b9285e0982994591216
Rasel-Shikder <rasel.sikder777.rk@gmail.com>
2026-04-13 19:43:14 +0600

    fixed - backend task 2

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use('/api', router)
+
 const PORT = process.env.PORT || 5000;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://mongodb:mongodb@cluster0.em4cgxh.mongodb.net/testing-jobs1?retryWrites=true&w=majority&appName=Cluster0");
     console.log(`App is connected to the database.`);
   } catch (error) {
     console.error(`Error connecting to DB: ${error.message}`);
